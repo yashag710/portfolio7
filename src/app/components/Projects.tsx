83eaffda7866490d198913ca10c9732f3cf7833f
YASH AGARWAL <143899665+yashag710@users.noreply.github.com>
2025-06-01 00:02:11 +0530

    Update Projects.tsx

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -56,7 +56,7 @@ const proj3 = [
 
 export default function ProjectsPage() {
   return (
-    <section className="bg-transparent py-16">
+    <section id="works" className="bg-transparent py-16">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="text-center mb-12">
